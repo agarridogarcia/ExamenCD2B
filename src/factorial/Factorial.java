344fca16fbd59f82c77e77496b281fcf0d60d1a0
Ara <Ara@DESKTOP-669DI6D>
2016-12-12 19:57:18 +0100

    cambio nombre variables

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -3,34 +3,35 @@ package factorial;
 
 
 public class Factorial {
+    //*Cambio nombre a las variables*//
 
 
     public static void main(String[] args) {
 
 
-    int j;
-    int f;
+    int numFactor;
+    int factorial;
 
   
-   j = 8;    
+   numFactor= 8;    
    
 
     
    int i;
-   if (j == 0)
-f = 1;
+   if (numFactor == 0)
+factorial= 1;
    else
    {
-f = 1;
-for (i = j; i >= 1; i--)
+factorial = 1;
+for (i = numFactor; i >= 1; i--)
 {
-    f = f * i;
+    factorial = factorial * i;
 }
    }    
   
  
 
-System.out.println(f); 
+System.out.println(factorial); 
 
 
     }
