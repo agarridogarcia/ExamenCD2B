a8978552eb0bf0ddfd93b10f14a7991e4144028e
Ara <Ara@DESKTOP-669DI6D>
2016-12-12 20:24:00 +0100

    llamo al metodo calcular

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -5,12 +5,14 @@ package factorial;
 public class Factorial {
     //Cambio nombre a las variables
     //Creo nueva clase y nuevo metodo al que llamo calcular
+    //llamo al metodo calcular desde la clase principal
     
     
 
 
     public static void main(String[] args) {
-   
+    Calcular calc=new Calcular();
+    calc.calculo();
 
 
     }
